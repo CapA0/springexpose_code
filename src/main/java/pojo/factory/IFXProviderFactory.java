bcef9911aeaf556269fc235d1e94ec984aaa6935
kingmore96 <wgg9696@163.com>
2018-11-30 10:07:18 +0800

    增加工厂方法注入实例

---
+++ src/main/java/pojo/factory/IFXProviderFactory.java
@@ -0,0 +1,13 @@
+package pojo.factory;
+
+import pojo.FXNewsProvider;
+import pojo.IFXNewsProvider;
+
+/**
+ * 实例工厂类，通过工厂实例获取Provider实例
+ */
+public class IFXProviderFactory {
+    public IFXNewsProvider getInstance(){
+        return new FXNewsProvider();
+    }
+}
